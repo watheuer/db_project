2a5a879000a08a9ed1d6426ef601cbbf598fbb57
Greg Wicks <gpwicks@email.wm.edu>
2016-04-12 14:17:35 -0400

    Fix model. Add admin integration.

--- champion_picker/admin.py
+++ champion_picker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from champion_picker.models import *
 
 # Register your models here.
+admin.site.register(Champion)
+admin.site.register(Role)
--- champion_picker/models.py
+++ champion_picker/models.py
@@ -9,22 +9,19 @@ class Champion(models.Model):
 
 class Role(models.Model):
     name = models.CharField(max_length=32)
-    champion = models.ForeignKey(
-        'Champion',
-        on_delete=models.SET_NULL
-    )
-    win_rate = models.DecimalField(decimal_places=2)
+    champion = models.ForeignKey(Champion, null=True, on_delete=models.SET_NULL)
+    win_rate = models.DecimalField(max_digits=30, decimal_places=2)
     kills = models.IntegerField()
     deaths = models.IntegerField()
     assists = models.IntegerField()
     minions_killed = models.IntegerField()
     q_skill = models.CharField(max_length=32)
-    w_skill = models.CharFIeld(max_length=32)
-    e_skill = models.CharFIeld(max_length=32)
-    r_skill = models.CharFIeld(max_length=32)
+    w_skill = models.CharField(max_length=32)
+    e_skill = models.CharField(max_length=32)
+    r_skill = models.CharField(max_length=32)
 
 class WinRate(models.Model):
-    champ1 = models.ForeignKey('Role')
-    champ2 = models.ForeignKey('Role')
-    win_rate = models.DecimalField(decimal_places=2)
+    champ1 = models.ForeignKey(Role, related_name='champ1')
+    champ2 = models.ForeignKey(Role, related_name='champ2')
+    win_rate = models.DecimalField(max_digits=30, decimal_places=2)
 
