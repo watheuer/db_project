fa4dd83044fcedf2241def9bddcc782f2f71f9cb
Greg Wicks <gpwicks@email.wm.edu>
2016-04-13 19:49:55 -0400

    More updates

--- champion_picker/admin.py
+++ champion_picker/admin.py
@@ -4,5 +4,6 @@ from champion_picker.models import *
 # Register your models here.
 admin.site.register(Champion)
 admin.site.register(Role)
+admin.site.register(WinRate)
 admin.site.register(ItemBuild)
 admin.site.register(Item)
--- champion_picker/models.py
+++ champion_picker/models.py
@@ -6,6 +6,10 @@ from django.db import models
 class Champion(models.Model):
     name = models.CharField(max_length=32)
     portrait_image = models.ImageField(upload_to='champ_images', blank=True, null=True)
+    q_skill = models.CharField(max_length=32)
+    w_skill = models.CharField(max_length=32)
+    e_skill = models.CharField(max_length=32)
+    r_skill = models.CharField(max_length=32)
     # other shit like the image
     def __str__(self):
         return self.name
@@ -18,10 +22,6 @@ class Role(models.Model):
     deaths = models.IntegerField()
     assists = models.IntegerField()
     minions_killed = models.IntegerField()
-    q_skill = models.CharField(max_length=32)
-    w_skill = models.CharField(max_length=32)
-    e_skill = models.CharField(max_length=32)
-    r_skill = models.CharField(max_length=32)
 
     def __str__(self):
         return "[%s] %s" % (self.name, self.champion)
@@ -30,6 +30,9 @@ class WinRate(models.Model):
     champ1 = models.ForeignKey(Role, related_name='champ1')
     champ2 = models.ForeignKey(Role, related_name='champ2')
     win_rate = models.DecimalField(max_digits=30, decimal_places=2)
+
+    def __str__(self):
+        return "%s %s" % (self.champ1, self.champ2)
 
 class Item(models.Model):
     name = models.CharField(max_length=48)
