b06bf968cb2fbd96479864ce7c766291f1e40dff
Greg Wicks <gpwicks@email.wm.edu>
2016-04-13 20:21:21 -0400

    Add import export

--- champion_picker/admin.py
+++ champion_picker/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
+from import_export.admin import ImportExportMixin, ExportMixin
 from champion_picker.models import *
 
+class ChampionAdmin(ImportExportMixin, admin.ModelAdmin):
+    class Meta:
+        model = Champion
+
 # Register your models here.
-admin.site.register(Champion)
+admin.site.register(Champion, ChampionAdmin)
 admin.site.register(Role)
 admin.site.register(WinRate)
 admin.site.register(ItemBuild)
