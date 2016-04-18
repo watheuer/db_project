5f10b62c8de69044fed7d60097418a8b86ac121b
JaytheDK <jchobbs@email.wm.edu>
2016-04-18 14:58:38 -0400

    Update champion.py
    Fixed up the image download thing so that it actually works and added the image locations to the csv file.

--- scripts/champion.py
+++ scripts/champion.py
@@ -5,6 +5,7 @@
 #Doing that import thing. This is the library for requesting the stuff, opening the response
 #       that has the stuff, and handling errors if it didn't want to give us the stuff.
 from urllib2 import Request, urlopen, URLError
+from urllib import urlretrieve
 #Import for creating a directory to store the files in so that we don't end up with a
 #       bunch of messy csv files every time we run this thing.
 from os import path, makedirs, getcwd
@@ -12,7 +13,8 @@ import csv
 
 riottoken = '180d011c-06d3-4722-9eef-3a3de8d6bc83' #My api token for riotgames
 ggtoken = 'ad7c8fe0625bea8067333db89d12dedc' #My api token for champion.gg
-folderpath = '/CSV Files' #Local folder for csv files
+csvpath = '/CSV Files' #Local folder for csv files
+imagepath = '/images'
 riotroot = 'https://na.api.pvp.net/api'
 ggroot = 'http://api.champion.gg'
 
@@ -28,10 +30,10 @@ fieldskillnames = ['name','image_location', 'q_skill', 'w_skill', 'e_skill', 'r_
 #Exact location of url for images
 urlimages = '/lol/static-data/NA/v1.2/champion'
 
-if not path.exists(getcwd()+folderpath):
-    makedirs(getcwd()+folderpath)
+if not path.exists(getcwd()+csvpath):
+    makedirs(getcwd()+csvpath)
 
-csvfile = open(getcwd()+folderpath+'/champion.csv', 'wb')
+csvfile = open(getcwd()+csvpath+'/champion.csv', 'wb')
 wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
 tothecsv = [[]]
@@ -45,6 +47,17 @@ response = urlopen(request).read() #opens the stuff for reading
 for item in response.split('key":"')[1:]:#prepares for iteration, strips off first index because I like my lists naked from the waist up
     champnameslist.append(item[:item.find('"')])#the name is surrounded by " so it slices before the second "
 
+download = raw_input("Download images as well? [y/n]\n")
+
+if download == 'y' or download == 'Y':
+    if not path.exists(getcwd()+imagepath):
+        makedirs(getcwd()+imagepath)
+    for name in champnameslist:
+        urlretrieve('http://ddragon.leagueoflegends.com/cdn/6.7.1/img/champion/{0}.png'.format(name),
+                    getcwd() + imagepath + '/{0}.png'.format(name))
+    print 'Images downloaded to folder.'
+
+    
 i=0
 for name in champnameslist:
     if i % 5 == 0:
@@ -54,6 +67,7 @@ for name in champnameslist:
     responseskills = urlopen(requestskills).read().split('name":"')[1:]
     temp = []
     temp.append(name)
+    temp.append(imagepath + '/' + name + '.png')
     temp.append("")
     for item in responseskills:
         temp.append(item[:item.find('"')])
