273b5eac490bb94330afac82acde4432245895a6
Greg Wicks <gpwicks@email.wm.edu>
2016-04-24 23:06:22 -0400

    Derp

--- champion_picker/views.py
+++ champion_picker/views.py
@@ -103,8 +103,8 @@ def get_matchup(request):
         entry_line = []
         srole = ""
         for c in m:
-            entry_line.append({"role1": c.role1.champion.name, "role2": c.role2.champion.name, "portrait": c.role1.champion.portrait_image.name, "win_rate": float(c.win_rate)})
-            srole = c.role2.champion.name
+            entry_line.append({"role_name": c.role1.name, "role1": c.role1.champion.name, "role2": c.role2.champion.name, "portrait": c.role1.champion.portrait_image.name, "win_rate": float(c.win_rate)})
+            srole = str(c.role2.name) + ":" + str(c.role2.champion.name)
         final_data[str(srole)] = entry_line
 
     return HttpResponse(json.dumps(final_data))
